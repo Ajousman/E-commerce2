667f3e36d598d7bcff7167610a15f9aa43ec0f0f
Ajousman <abdu0932hu@gmail.com>
2025-10-31 05:54:16 -0700

    your commit message

---
+++ src/Pages/Product.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import Hero from '../Components/Hero/Hero'; // or correct path to Hero component
+import { ShopContext } from '../Context/ShopContext';
+import {useParams} from "react-router-dom";
+import Items from '../Components/Items/Items';
+import BreadCrum from '../Components/BreadCrums/BreadCrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+const Product = () => {
+  const{all_product}=useContext(ShopContext);
+  const {productId}=useParams();
+  const product=all_product.find((e)=>e.id===Number(productId))
+  return (
+    <div>
+     <BreadCrum product={product}/> 
+     <ProductDisplay product={product}/>
+    </div>
+  )
+}
+
+export default Product
