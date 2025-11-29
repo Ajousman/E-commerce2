dbfb26baa2c78f1b81583499b687cff7dfc23194
Ajousman <154000913+Ajousman@users.noreply.github.com>
2025-11-28 22:04:22 -0800

    Update fetch URL for new collections data

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -5,7 +5,7 @@ import Item from '../Items/Items'
 const NewCollections = () => {
   const [new_collection,setNew_collection]=useState([]);
   useEffect(()=>{
-  fetch('http://localhost:4000/newcollections')
+  fetch('https://e-commerce-backend-00fz.onrender.com/newcollections')
   .then((response)=>response.json())
   .then((data)=>setNew_collection(data));
   },[])
