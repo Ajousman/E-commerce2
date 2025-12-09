a6bc79b8dd6869034100bef2c850c159f61055e1
Ajousman <abdu0932hu@gmail.com>
2025-12-08 23:12:35 -0800

    Fix: Add frontend as regular folder

---
+++ frontend/src/Components/Popular/Popular.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import './Popular.css'
+//import data_product from '../Navbar/Assets/Frontend_Assets/data'
+import Item from '../Items/Items'   // assuming you have an Item component
+
+const Popular = () => {
+  const[popularproducts,setPopularProducts]=useState([]);
+  useEffect(()=>{
+    fetch('https://e-commerce-backend-00fz.onrender.com/popularinwomen')
+    .then((response)=>response.json())
+    .then((data)=>setPopularProducts(data))
+  },[]);
+  return (
+    <div className='popular'>
+      <h1>POPULAR IN WOMEN</h1>
+      <hr/>
+      <div className="popular-item">
+        {popularproducts && popularproducts.map((item, i) => (
+          <Item
+            key={item.id ?? i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Popular
---
+++ frontend/src/Context/ShopContext.jsx
@@ -0,0 +1,95 @@
+import react ,{createContext, useEffect, useState} from 'react'
+//import all_product from '../Components/Navbar/Assets/Frontend_Assets/all_product'
+
+
+export const ShopContext=createContext(null);
+const getDefaultCart=()=>{
+        let cart={};
+        for(let index=0; index<300+1;index++){
+            cart[index]=0;
+        }
+        return cart;
+    }
+const ShopContextProvider=(props)=> {
+  const[all_product,setAll_product]=useState([]);
+  const [cartItems,setCartItems]=useState(getDefaultCart());
+  useEffect(()=>{
+   fetch('https://e-commerce-backend-00fz.onrender.com/allproducts')
+   .then((response)=>response.json())
+   .then((data)=>setAll_product(data))
+   if(localStorage.getItem('auth-token')){
+    fetch('http://https://e-commerce-backend-00fz.onrender.com/getcart',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'auth-token':`${localStorage.getItem('auth-token')}`,
+        'Content-Type':'application/json',
+      },
+      body:""
+    })
+    .then((response)=>response.json())
+    .then((data)=>setCartItems(data));
+   }
+  },[])
+   
+  const addToCart=(itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+   if(localStorage.getItem('auth-token')){
+    fetch('https://e-commerce-backend-00fz.onrender.com/addtocart',{
+      method:'POSt',
+      headers:{
+        Accept:'application/form-data',
+        'auth-token':`${localStorage.getItem('auth-token')}`,
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({"itemId":itemId}),
+    })
+    .then((response)=>response.json())
+    .then((data)=>console.log(data));
+   }
+  }
+    const removeFromCart=(itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+   if(localStorage.getItem('auth-token')){
+     fetch('https://e-commerce-backend-00fz.onrender.com/removefromcart',{
+      method:'POST',
+      headers:{
+        accept:'application/form-data',
+        'auth-token':`${localStorage.getItem('auth-token')}`,
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({"itemId":itemId}),
+    })
+    .then((response)=>response.json())
+    .then((data)=>console.log(data));
+   }
+  }
+  const getTotalCartAmount =() =>{
+    let totalAmount=0;
+    for(const item in cartItems)
+      {
+      if(cartItems[item]>0)
+        {
+        let itemInfo=all_product.find((product)=>product.id===Number(item));
+        totalAmount +=itemInfo.new_price * cartItems[item];
+      }
+     
+    }
+     return totalAmount;
+  }
+  const getTotalCartItem=()=>{
+    let totalItem=0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+        totalItem +=cartItems[item];
+      }
+    }
+    return totalItem;
+  }
+   const contextValue={getTotalCartItem,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
+    return(
+        <ShopContext.Provider value={contextValue}>{props.children} 
+        </ShopContext.Provider>
+    )
+}
+export default ShopContextProvider;
